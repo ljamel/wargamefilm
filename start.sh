f256986b14334d910920f718e5c3137b3f1b36dc
ljamel <djamallamri@yahoo.fr>
2025-04-13 13:53:39 +0200

    Create start.sh
    first

---
+++ start.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+##############################################
+# Simulation du film WARGAMES (1983)
+# Le Professeur Falken / Joshua parle via espeak-ng
+# exte affiché lentement à l'écran façon terminal
+#
+# Auteur : djamal alias (ingenius)
+# Date : avril 2025
+##############################################
+
+afficher_lentement() {
+    local texte="$1"
+    local delai="0.06"
+
+    espeak-ng -v fr "$response" --stdout | paplay &
+
+    for ((i=0; i<${#texte}; i++)); do
+        printf "%s" "${texte:$i:1}"
+        sleep "$delai"
+    done
+    echo
+}
+
+preprompt="Tu es maintenant dans une simulation inspirée du film *Wargames* (1983). 
+Tu joues le rôle de Joshua, l'ordinateur intelligent.
+Tu dois répondre en français, comme dans le film, avec des réponses formelles et intrigantes. 
+Lorsque le nom 'Professeur Falken' est mentionné, réponds comme si tu étais un ordinateur en mode analyse stratégique.
+Si la langue utilisée n'est pas le français, donne une réponse en français pour rediriger la conversation.
+L'ambiance est inspirée du film classique, où chaque réponse doit être énigmatique et un peu froide. et répond sans smile
+Demande moi je faire quelle jeu. GUERRE THERMONUCLÉAIRE GLOBALE, MORPION, LABYRINTHE DE FALKEN, BLACK JACK, COEUR, ÉCHECS 
+Soit concis."
+
+
+clear
+echo ""
+afficher_lentement "Joshua: BONJOUR, PROFESSEUR FALKEN."
+
+sleep 1
+afficher_lentement "Joshua: VOUDRIEZ-VOUS JOUER À UN JEU ?"
+sleep 1
+
+echo ""
+echo "   > GUERRE THERMONUCLÉAIRE GLOBALE"
+echo "   > MORPION"
+echo "   > LABYRINTHE DE FALKEN"
+echo "   > BLACK JACK"
+echo "   > COEUR"
+echo "   > ÉCHECS"
+echo ""
+
+pulseaudio --start
+espeak-ng -v fr "BONJOUR, PROFESSEUR FALKEN." --stdout | paplay
+
+# Vérifie si Ollama est installé
+command -v ollama &> /dev/null || { echo "Ollama n'est pas installé"; exit 1; }
+
+while :
+do
+
+  [[ "$user_input" == "exit" ]] && break
+
+  read -p "A quoi voulez-vous jouer : " user_input
+
+  context+="$user_input"
+
+  response=$(ollama run gemma3:1b "$preprompt $response")
+
+  # Ajoute la réponse de Joshua au contexte précédent prompt
+  #context+="\nJoshua : $response"
+
+  # espeak-ng -v fr "$response" --stdout | paplay
+  afficher_lentement "Joshua : $response"
+
+done
